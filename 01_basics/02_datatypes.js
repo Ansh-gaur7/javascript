e919c0e392b4acd0c26bc166e47bd86f107aa52a
Ansh-gaur7 <anshgaur0582@gmail.com>
2026-07-21 16:29:58 +0000

    learn datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Hitesh")
+
+// ab ham niche abhi primitive datatype dekhenge
+let name="ansh"//string
+let age=40 //number
+let logIn=true//boolean
+
+//numbers ->2 to the power 53 range hai iski
+//boolean-> true/false
+//string-> " "
+//bigint
+//null -> standalone value
+//symbol->unique
+//undefined
+console.log(typeof null)//output-> object
+console.log(typeof undefined);//output->undefined
+// object = ye primitive nhi hai
+/* through the typeof we can identify the datatype
+
+*/
+
