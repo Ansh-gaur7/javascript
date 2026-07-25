cf89a8ef7218a7bf8db064a136000decf2424f56
Ansh-gaur7 <anshgaur0582@gmail.com>
2026-07-25 17:47:25 +0000

    some notes added

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -2,18 +2,18 @@ const user = {
     username: "hitesh",
     price: 999,
 
-    welcomeMessage: function() {
+    welcomeMessage: function () {
         console.log(`${this.username} , welcome to website`);
-        console.log(this);
+        console.log(this);//this is generally use to execute current context(current variables)
     }
 
 }
 
-// user.welcomeMessage()
+user.welcomeMessage()
 // user.username = "sam"
 // user.welcomeMessage()
 
-// console.log(this);
+// console.log(this);//here this gives output{} because it have global context
 
 // function chai(){
 //     let username = "hitesh"
@@ -24,7 +24,7 @@ const user = {
 
 // const chai = function () {
 //     let username = "hitesh"
-//     console.log(this.username);
+//     console.log(this.username);// this does not work in functions
 // }
 
 const chai =  () => {
@@ -33,17 +33,17 @@ const chai =  () => {
 }
 
 
-// chai()
+//chai()
 
 // const addTwo = (num1, num2) => {
-//     return num1 + num2
+//     return num1 + num2  //explicit return
 // }
 
 // const addTwo = (num1, num2) =>  num1 + num2
 
 // const addTwo = (num1, num2) => ( num1 + num2 )
 
-const addTwo = (num1, num2) => ({username: "hitesh"})
+const addTwo = (num1, num2) => ({username: "hitesh"})//implicit return here we dont need to write return keyword but if we apply curly braces then it will be must to write curly braces
 
 
 console.log(addTwo(3, 4))
